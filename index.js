2c83b98a45cf07b9b10964d764338666c54a8cae
Siege <81945479+SiegeToaster@users.noreply.github.com>
2021-10-13 18:14:30 -0700

    simrad files added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ console.log('TI Calculator Programs\nBy Siege\n');
 console.log('0: Area Calculation\n1: Distance\n2: Factoring Quadratics\n3: Pythagorean Calculator\n4: Quadratic Formula\n5: Simplify Radicals\n6: Triangle\n7: Volume Calculation\n');
 
 import pythag from './programs/Pythagorean Calculator/pythag.js';
+import simrad from './programs/Simplify Radicals/simrad.js';
 
 const rl = readline.createInterface({
 	input: process.stdin,
@@ -35,7 +36,7 @@ rl.question('Select Program: ', (programNumber) => {
 		break;
 
 	case '5':
-		console.log('Work In Progress');
+		simrad();
 		break;
 
 	case '6':
